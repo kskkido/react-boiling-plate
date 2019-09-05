8c3879f3796f1b9a2c22057700123ca913113cfa
Keisuke Kido <kskkido@KeisukenoMacBook-puro.local>
2019-09-05 16:28:19 +0900

    fix env server

--- server/middleware/assets/index.js
+++ server/middleware/assets/index.js
@@ -6,6 +6,3 @@ const router = express.Router();
 
 module.exports = router
   .use(config.publicPath, express.static(config.outputPath))
-  .get('*', (_, res) => {
-    res.sendFile(path.join(config.outputPath, 'index.html'));
-  });
